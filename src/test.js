908d2edfdb516056a826ee8b3781d2e74a8f7faf
TR <2993078435@qq.com>
2023-12-04 15:33:33 +0800

    清除缓存再次部署

--- src/test.js
+++ src/test.js
@@ -1,70 +1,73 @@
-import string from "./css.js"
+import string from './css.js'
+
 
 const player = {
-    id:undefined,
-    n:1,
-    time:100,
-    ui:{
-        demo :document.querySelector('#demo'),
-        demo2 :document.querySelector('#demo2')
-    },
-    events : {
-        '#btnPause':'pause',
-        '#btnPlay':'play',
-        '#btnSlow':'slow',
-        '#btnNormal':'normal',
-        '#btnFast': 'fast'
-    },
-    init:()=>{
-        player.ui.demo.innerText = string.substring(0,player.n)
-        player.ui.demo2.innerHTML = string.substring(0,player.n)
-        player.bindEvents()
-        player.play()
-        
-    },
-    bindEvents : ()=>{
-        for(let key in player.events){
-            if(player.events.hasOwnProperty(key)){
-                const value = player.events[key]
-                document.querySelector(key).onclick = player[value]
-    
-            }
-        }
-    },
-    run :()=>{
-        if(player.n>string.length){
-            window.clearInterval(player.id)
-            return
-        }
-        player.n+=1
-        player.ui.demo.innerText = string.substring(0,player.n)
-        player.ui.demo2.innerHTML = string.substring(0,player.n)
-        player.ui.demo.scrollTop = player.ui.demo.scrollHeight
-    },
-    play : ()=>{
-        player.id = setInterval(player.run,player.time)
-    },
-    pause : ()=>{
-        window.clearInterval(player.id)
-    },
-    slow : ()=>{
-        player.time = 300
-        player.pause()
-        player.play()
-    },
-     normal : ()=>{
-        player.time = 100
-        player.pause()
-        player.play()
-    },
-     fast : ()=>{
-        player.time = 10
-        player.pause()
-        player.play()
+  id: undefined,
+  time: 100,
+  ui: {
+    demo: document.querySelector('#demo'),
+    demo2: document.querySelector('#demo2')
+  },
+  events: {
+    '#btnPause': 'pause',
+    '#btnPlay': 'play',
+    '#btnSlow': 'slow',
+    '#btnNormal': 'normal',
+    '#btnFast': 'fast'
+  },
+  n: 1,
+  init: () => {
+    player.ui.demo.innerText = string.substr(0, player.n)
+    player.ui.demo2.innerHTML = string.substr(0, player.n)
+    player.bindEvents()
+    player.play()
+  },
+  bindEvents: () => {
+    for (let key in player.events) {
+      if (player.events.hasOwnProperty(key)) {
+        const value = player.events[key] // pause / play / slow
+        document.querySelector(key).onclick = player[value]
+      }
     }
-    
+
+  },
+  run: () => {
+    player.n += 1
+    if (player.n > string.length) {
+      window.clearInterval(player.id)
+      return
+    }
+    player.ui.demo.innerText = string.substr(0, player.n)
+    player.ui.demo2.innerHTML = string.substr(0, player.n)
+    player.ui.demo.scrollTop = player.ui.demo.scrollHeight
+  },
+  play: () => {
+    window.clearInterval(player.id)
+    player.id = setInterval(player.run, player.time)
+  },
+  pause: () => {
+    window.clearInterval(player.id)
+  },
+  slow: () => {
+    player.pause()
+    player.time = 300
+    player.play()
+  },
+  normal: () => {
+    player.pause()
+    player.time = 100
+    player.play()
+  },
+  fast: () => {
+    player.pause()
+    player.time = 0
+    player.play()
+  }
 }
+
 player.init()
 
- 
+
+
+
 
